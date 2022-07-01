5ebe3c2a1c47f7d51c94223592db7352c285c55a
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-07-01 20:11:00 -0300

    all sys

--- start_job.py
+++ start_job.py
@@ -2,10 +2,10 @@ import asyncio
 
 from app.db import client, teste
 from app.loadfile import get_in_quee
-from app.loadpoint import get_point
+from app.loadpoint_new import get_point
 
 if '__main__' == __name__:
 
     loop = client.get_io_loop()
-    # loop.run_until_complete(get_in_quee())
+    loop.run_until_complete(get_in_quee())
     loop.run_until_complete(get_point())
