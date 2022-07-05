16a97466ffdd298c6c294506446c846bf6a4baad
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-07-04 21:53:42 -0300

    update fetures good

--- app/loadfile.py
+++ app/loadfile.py
@@ -4,10 +4,10 @@ import geopandas as gpd
 from geopandas.tools import sjoin
 
 from app.config import logger, settings
-from app.db import PyObjectId, db_features, db_dataset
+from app.db import ObjectId, db_features, db_dataset
 from app.model.functions import get_id_by_lon_lat
 from multiprocessing import Pool, cpu_count
-from pymongo import MongoClient, UpdateOne
+from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 
 regions_tmp = gpd.read_file('zip:///data/base/regions.zip').add_prefix(
@@ -28,7 +28,7 @@ def create_feture(args):
     row, epsg = args
     lon, lat = row['geometry']['coordinates']
     properties = row['properties']
-    _id = PyObjectId(properties.pop('__pgrass_gid'))
+    _id = {'_id': ObjectId(properties.pop('__pgrass_gid'))}
     root = {
         'biome': properties['regions_BIOMA'],
         'municipally': properties['regions_MUNICIPIO'],
@@ -37,8 +37,9 @@ def create_feture(args):
         'lon': lon,
         'point_id':get_id_by_lon_lat(lat,lon, epsg)
     }
+    
     logger.debug(f'update in db {_id}')
-    return UpdateOne({'_id':_id}, {'$set':root})
+    return (_id, {'$set':root})
 
 def __add_infos_to_doc__(dataset_id,docs,columns,epsg ):
     regions_espg = regions.to_crs(epsg)
@@ -48,35 +49,12 @@ def __add_infos_to_doc__(dataset_id,docs,columns,epsg ):
     gdf = gdf.set_crs(epsg)
 
     df_join = sjoin(gdf, regions_espg)
-    with Pool(cpu_count()* 2) as work:   
+    with Pool(cpu_count()-1) as work:   
         result = work.map(create_feture, [(row,epsg ) for row in df_join.iterfeatures()])
     
     return  result
     
 
-
-def save_in_db(save):
-    logger.debug(f"save in db {save}")
-    client = MongoClient(settings.MONGODB_URL,maxPoolSize=10000)
-    db = client.pgrss
-    db.features.update_one(*save)
-
-    
-def list_to_matrix(informations):
-  matrix = []
-  row = []
-  for i, information in enumerate(informations):
-    if i == 0 or i % 10000:
-      row.append(information)
-    else:
-      row.append(information)
-      matrix.append(row)
-      row = []
-  if len(row) > 0:
-    matrix.append(row)
-    row = []
-
-  return matrix
 
 async def get_in_quee():
     dataset_ids = await db_features.distinct(
@@ -91,16 +69,14 @@ async def get_in_quee():
                     **dataset}}
                 async for doc in db_features.find({'dataset_id':dataset_id,'municipally': {'$exists': False}}):
                     document[dataset_id]['docs'].append(doc)
+                
+                
                 feature_update =__add_infos_to_doc__(dataset_id,**document[dataset_id])
-                client = MongoClient(settings.MONGODB_URL,maxPoolSize=10000)
-                db = client.pgrss
-                try:
-                    result =  db.features.bulk_write(feature_update)
-                    logger.info(f'Save bulk in db {result.bulk_api_result}')
-                except BulkWriteError as bwe:
-                    logger.exception(bwe.details)
                 
-                
+                for _id, values in feature_update:
+                    logger.debug(f'save in db {_id}')
+                    await db_features.update_one(_id,values)
+                    
                 
             except Exception as e:
                 logger.exception(f'Error in loadfile {e}')
