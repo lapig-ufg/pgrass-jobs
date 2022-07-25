799471a0b12a8e2a1dfed70b2890c175c0f00620
Jairo Matos da Rocha <jairo@discente.ufg.br>
2022-07-25 18:13:28 -0300

    bug np array fix

--- app/sattelite/sentinel2.py
+++ app/sattelite/sentinel2.py
@@ -25,10 +25,11 @@ def __transformer(lon,lat, point_epsg,raster_epsg):
 def read_pixel(asset, _datetime, url, lon, lat, epsg): 
     with rasterio.open(url) as ds:
         lon_t, lat_t = __transformer(lon, lat, epsg, ds.crs['init'])
-        pixel_val = next(ds.sample([(lon_t, lat_t)]))
+        pixel_val = (next(ds.sample([(lon_t, lat_t)]))).tolist()
         logger.debug(f"band:{asset}, cog:{url}, cood([{lon}, {lon_t} ], [{lat}, {lat_t} ], {epsg}), pixel:{pixel_val}")
         if len(pixel_val) == 1:
             pixel_val = pixel_val[0]
+        
         return {
             'asset': asset,
             'datetime': _datetime,
